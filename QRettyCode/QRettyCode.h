5469f9b891a37d785c4150a41659204cadc8139c
Michael Redig <mredig@gmail.com>
2019-11-13 23:32:51 -0600

    added project and gitignore

---
+++ QRettyCode/QRettyCode.h
@@ -0,0 +1,19 @@
+//
+//  QRettyCode.h
+//  QRettyCode
+//
+//  Created by Michael Redig on 11/13/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRettyCode.
+FOUNDATION_EXPORT double QRettyCodeVersionNumber;
+
+//! Project version string for QRettyCode.
+FOUNDATION_EXPORT const unsigned char QRettyCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRettyCode/PublicHeader.h>
+
+
