4f2a8f76a513e7f1e7822bdb720b53e88d002d44
Michael Redig <mredig@gmail.com>
2020-03-29 03:57:25 -0500

    (feat) marked minimum platform compatibility

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "QRettyCode",
+	platforms: [
+		.iOS(.v10),
+		.tvOS(.v10),
+	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
